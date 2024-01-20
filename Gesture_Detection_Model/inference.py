05d7755a455990d2580f94aa0368267fc632b253
Alexiy Buynitsky <abuynits@gmail.com>
2024-01-20 05:22:57 -0500

    I UNZQEEEEEEEEEEEEZED A NUMPY ARRAY AND IT WORKED

--- Gesture_Detection_Model/inference.py
+++ Gesture_Detection_Model/inference.py
@@ -55,8 +55,7 @@ def calc_landmark_list(image, landmarks):
 
 class MLP_Inference(object):
     def __init__(self, threads=1, output_classes=config["output_classes"]):
-        self.model = load_MLP(output_classes)
-        self.model.load_state_dict(torch.load(config["model_save_path"]))
+        self.model = torch.load(config["model_save_path"])
         torch.set_num_threads(threads)
 
     def __call__(
@@ -64,5 +63,8 @@ class MLP_Inference(object):
             landmark_list,
     ):
         inp = torch.tensor(landmark_list, dtype=torch.float32)
+        inp = inp.unsqueeze(0)
         out = self.model(inp)
-        return np.argmax(np.squeeze(out))
+        out = out.squeeze(0)
+        print(out)
+        return np.argmax(np.squeeze(out.detach().numpy()))
