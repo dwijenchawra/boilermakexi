5143b0a7e14edc5b5df5e1dcacabdd54756effa1
Alexiy Buynitsky <abuynits@gmail.com>
2024-01-20 00:44:43 -0500

    feat: added gesture rmse match logic

--- Gesture_storage/Static_gesture.py
+++ Gesture_storage/Static_gesture.py
@@ -8,17 +8,28 @@ def init_gesture_from_values(endpoints, score, label):
     return Static_gesture(d)
 
 
+def get_dist(p1, p2):
+    return math.sqrt((p1["x"] - p2["x"]) * (p1["x"] - p2["x"]) + (p1["y"] - p2["y"]) * (p1["y"] - p2["y"]))
+
+
 class Static_gesture():
     def __init__(self, d):
         self.points = d["endpoints"]
         self.score = d["score"]
         self.label = d["label"]
 
-    def xy_distance(self, x, y):
-        return math.sqrt((x - self.x)(x - self.x) + (y - self.y) * (y - self.y))
-
     def get_json_data(self):
         d = {"endpoints": self.points,
              "score": self.score,
              "label": self.label}
         return d
+
+    def get_rmse(self, g):
+        all_dist = 0.
+        for i in range(len(self.points)):
+            p1 = self.points[i]
+            p2 = g.points[i]
+            all_dist += get_dist(p1, p2)
+        return all_dist / len(self.points)
+
+        # loop over
