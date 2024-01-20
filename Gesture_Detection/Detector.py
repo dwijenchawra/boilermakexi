407750c5b65bef85922a7aa3858cc7b7ca022400
Alexiy Buynitsky <abuynits@gmail.com>
2024-01-20 12:35:16 -0500

    feat: removed garbage

--- Gesture_Detection/Detector.py
+++ Gesture_Detection/Detector.py
@@ -8,24 +8,27 @@ class ZDirection(Enum):
     N_A = 2
 
 class Rotation(Enum):
+   N_A = 0
+   CCW = 1
+   CW = 2
 
-   CCW = 0
-   CW = 1
-class Color(Enum):
+class Translation(Enum):
     N_A = 0
     LEFT = 1
     UP = 2
     DOWN = 3
     RIGHT = 4
-    INTO_SCREEN = 5
-    OUT_OF_SCREEN = 6
 
 
 config = load_config()
 
+
 class Detector:
     def __init__(self, time_delta):
         self.q = Queue(maxsize=time_delta)
+        self.rotation = Rotation.N_A
+        self.translation = Translation.N_A
+        self.depth = ZDirection.N_A
         # plot a rectangle for the points
 
 
