1aa68d6780a7e6ff25d8fc84881035d3109ec3b4
Alexiy Buynitsky <abuynits@gmail.com>
2024-01-20 09:08:14 -0500

    minor chngaes

--- Gesture_Detection_Model/inference.py
+++ Gesture_Detection_Model/inference.py
@@ -11,30 +11,30 @@ config = load_config()
 
 
 def pre_process_landmark(landmark_list):
-    temp_landmark_list = copy.deepcopy(landmark_list)
+    tmp = copy.deepcopy(landmark_list)
 
     # Convert to relative coordinates
     base_x, base_y = 0, 0
-    for index, landmark_point in enumerate(temp_landmark_list):
+    for index, landmark_point in enumerate(tmp):
         if index == 0:
             base_x, base_y = landmark_point[0], landmark_point[1]
 
-        temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
-        temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+        tmp[index][0] = tmp[index][0] - base_x
+        tmp[index][1] = tmp[index][1] - base_y
 
     # Convert to a one-dimensional list
-    temp_landmark_list = list(
-        itertools.chain.from_iterable(temp_landmark_list))
+    tmp = list(
+        itertools.chain.from_iterable(tmp))
 
     # Normalization
-    max_value = max(list(map(abs, temp_landmark_list)))
+    max_value = max(list(map(abs, tmp)))
 
     def normalize_(n):
         return n / max_value
 
-    temp_landmark_list = list(map(normalize_, temp_landmark_list))
+    tmp = list(map(normalize_, tmp))
 
-    return temp_landmark_list
+    return tmp
 
 
 def calc_landmark_list(image, landmarks):
@@ -42,13 +42,11 @@ def calc_landmark_list(image, landmarks):
 
     landmark_point = []
 
-    # Keypoint
     for _, landmark in enumerate(landmarks.landmark):
-        landmark_x = min(int(landmark.x * image_width), image_width - 1)
-        landmark_y = min(int(landmark.y * image_height), image_height - 1)
-        # landmark_z = landmark.z
+        x = min(int(landmark.x * image_width), image_width - 1)
+        y = min(int(landmark.y * image_height), image_height - 1)
 
-        landmark_point.append([landmark_x, landmark_y])
+        landmark_point.append([x, y])
 
     return landmark_point
 
